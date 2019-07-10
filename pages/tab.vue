3da284ad55a8eb07c67a6a636c790b8263747350
ccc949352432 <2427860813@qq.com>
2019-07-10 15:32:48 +0800

    项目初始化

---
+++ pages/tab.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="top">
+      <span
+        v-for="(item, index) in ['新闻', '体育', '军事']"
+        :key="index"
+        :class="{active:current===index}"
+        @click="handleClick(index)"
+      >{{ item }}</span>
+    </div>
+    <div v-if="current===0">
+      <ul>
+        <li>这是最近的新闻</li>
+        <li>这是最近的新闻</li>
+        <li>这是最近的新闻</li>
+      </ul>
+    </div>
+    <div v-if="current===1">
+      <ul>
+        <li>这是最近的体育</li>
+        <li>这是最近的体育</li>
+        <li>这是最近的体育</li>
+      </ul>
+    </div>
+    <div v-if="current===2">
+      <ul>
+        <li>这是最近的军事</li>
+        <li>这是最近的军事</li>
+        <li>这是最近的军事</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      current: 0
+    }
+  },
+  methods: {
+    handleClick(index) {
+      this.current = index
+    }
+  }
+}
+</script>
+
+<style>
+.top {
+  margin-bottom: 20px;
+}
+.top span {
+  cursor: pointer;
+  margin-left: 20px;
+}
+.active{
+  color:red
+}
+</style>
